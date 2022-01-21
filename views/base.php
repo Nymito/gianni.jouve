daf1851177cd9da22fcc9625f19b7795fcbf79a9
Nymito <gianni.jouve@gmail.com>
2022-01-21 04:10:25 +0100

    added slogan + img, added logo

--- views/base.php
+++ views/base.php
@@ -22,6 +22,9 @@ $recipes = [
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/7870449ae9.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap" rel="stylesheet">
     
     <title>Gianni Jouve</title>
 </head>
@@ -36,23 +39,33 @@ $recipes = [
         <div class="container">
             <div class="navbar">
                 <a href="/" class="nav-logo">
-                    Gianni Jouve
-                    <div class="logo-circle"></div>
                 </a>
                 <div class="nav-links">
-                    <a href="/" class="nav-item"><i class="fas fa-home"></i>Home</a>
                     <a href="/" class="nav-item">
-                        <span style="color: Mediumslateblue;">
+                        <span style="color: #FFE162; font-size: 1rem;" >
+                            <i class="fas fa-home"></i>
+                        </span>
+                        About Me
+                    </a>
+                    <a href="/" class="nav-item">
+                        <span style="color: #91C483; font-size: 1rem;">
                             <i class="fas fa-code"></i>
                         </span>
                         Portfolio
                     </a>
                     
-                    <a href="/" class="nav-item"><i class="fas fa-inbox"></i>Contact Me</a>
+                    <a href="/" class="nav-item">
+                        <span style="color: #FF6464; font-size: 1rem;">
+                            <i class="fas fa-inbox"></i>
+                        </span>
+                        Contact Me
+                    </a>
                 </div>
-                
-                
             </div>
+        </div>
+        <div class="header-slogan">
+            <div class="header-slogan-title">FULL-STACK DEVELOPER</div>
+            <img src="/public/img/cvPhoto.jpg" alt="">
         </div>
     </div>
 </body>

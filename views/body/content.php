e89321640e49f7ec07eee9e1bcb3d75b98e030b1
Nymito <gianni.jouve@gmail.com>
2022-02-02 01:35:11 +0100

    no ideas for the front ..., white page syndrom

--- views/body/content.php
+++ views/body/content.php
@@ -40,15 +40,21 @@
             </div>
         </div>
     </div>
+
     <div class="containerContent">
+        
+        <img src="/public/img/devUser.png" class="devUserImg">
         <div class="aboutMe">
             <div class="titleAbout">
                 ABOUT ME
             </div>
             <div class="titleLine"></div>
-            <img src="/public/img/devUser.png" class="devUserImg">
+            
             <div class="contentAbout">
-
+                I am a graduated engineer in computer sciences. Since more than 3 years, I have been hired as an apprentice in 2 huge companies.
+                Thales Alenia Space and Societe Generale.
+                I enjoy coding, it has been a part of my life for few years now.
+                Thank you for beeing here and do not forget to discover my Portfolio section to discover other projects I have worked on.
             </div>
         </div>
     </div>

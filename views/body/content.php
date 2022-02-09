84648d16ceb52c5a4d12ae4486bc49fa65ec63da
Nymito <gianni.jouve@gmail.com>
2022-02-09 01:48:06 +0100

    php try

--- views/body/content.php
+++ views/body/content.php
@@ -114,8 +114,31 @@
 
 </html>
 <?php
-    echo '<pre>';
+session_start();
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    http_response_code(500);
+    exit();
+}
 
-        print_r($_POST);
-    echo '</pre>';
+if (isset($_SESSION['count'])) $_SESSION['count']++;
+else $_SESSION['count'] = 0;
+if ($_SESSION['count'] == 3) {
+    http_response_code(403);
+    exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "remy93130@gmail.com";
+$subject = "Vous avez recu un message de : $name";
+$body = "Vous avez recu un nouveau message depuis le formulaire de votre site perso.\n\n"."Voici les details :\nNom : $name\n\nMail : $email\n\nMessage :\n$message";
+$header = "From: noreply@BarberetRemy.com\n";
+$header .= "Reply-To: $email";  
+
+if(!mail($to, $subject, $body, $header))
+    http_response_code(500);
+
 ?>
